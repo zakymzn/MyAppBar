fbba6e4b13a261a28f15a1c8a1fb424fda43b0e9
zakymzn <mamurzakynurrokhman@gmail.com>
2025-02-04 11:51:41 +0700

    latihan searchbar dan searchview

--- app/src/main/java/com/example/myappbar/MenuActivity.kt
+++ app/src/main/java/com/example/myappbar/MenuActivity.kt
@@ -1,20 +1,32 @@
 package com.example.myappbar
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.myappbar.databinding.ActivityMenuBinding
 
 class MenuActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMenuBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_menu)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        with(binding) {
+            searchView.setupWithSearchBar(searchBar)
+            searchView
+                .editText
+                .setOnEditorActionListener { textView, actionId, event ->
+                    searchBar.setText(searchView.text)
+                    searchView.hide()
+                    Toast.makeText(this@MenuActivity, searchView.text, Toast.LENGTH_SHORT).show()
+                    false
+                }
         }
     }
 }
